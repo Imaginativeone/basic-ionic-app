2817a7f7990c97aae226fe8b7b51618de638605c
Doug Franklin <doug@dougfranklin.com>
2024-05-01 05:01:26 -0400

    Loading data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react'
 import { SQLiteDBConnection } from '@capacitor-community/sqlite'
 import useSQLiteDB from '../composables/useSQLiteDB'
 
+import './Home.css'
+
 type SQLitem = {
   id: number
   name: string
@@ -13,8 +15,21 @@ const Home = () => {
   // SQLite DB Hook
   const { initialized, performSQLAction } = useSQLiteDB()
 
+  useEffect(() => {
+    loadData()
+  }, [initialized])
+
+  const loadData = async () => {
+    try {
+      console.log('Data is being loaded here...');
+    }
+    catch (e) {
+      console.log('Error loading data:', e)
+    }
+  }
+
   return (
-    <div>
+    <div className='homepage'>
       <h1>Home</h1>
     </div>
   );
